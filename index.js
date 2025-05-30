75107f9a4fd26a150981b55967b539514baa47a4
sridhar-thirumal <sridhar.thirumal@bigthinkcode.com>
2025-05-30 11:00:43 +0530

    Added x icon

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
-    document.querySelector(".ham-menu").onclick = function() {
+    document.querySelector(".menu").onclick = function() {
         document.querySelector("ul").style.display = 
             document.querySelector("ul").style.display === "none" ? "flex": "none";
+        if(document.querySelector(".ham-menu").style.display === "block") {
+            document.querySelector(".ham-menu").style.display = "none";
+            document.querySelector(".close-menu").style.display = "block";
+        }
+        else {
+            document.querySelector(".ham-menu").style.display = "block";
+            document.querySelector(".close-menu").style.display = "none";
+        }
     };
 }
