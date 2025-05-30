8099f23bb3832a9f1fa638ebac9185a9b5b3344d
sridhar-thirumal <sridhar.thirumal@bigthinkcode.com>
2025-05-30 12:28:58 +0530

    New asset

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", main);
 
 function main() {
     document.querySelector(".menu").onclick = function() {
-        document.querySelector("ul").style.display = 
-            document.querySelector("ul").style.display === "none" ? "flex": "none";
         if(document.querySelector(".ham-menu").style.display === "block") {
             document.querySelector(".ham-menu").style.display = "none";
             document.querySelector(".close-menu").style.display = "block";
+            document.querySelector("ul").style.display = "flex";
         }
         else {
             document.querySelector(".ham-menu").style.display = "block";
             document.querySelector(".close-menu").style.display = "none";
+            document.querySelector("ul").style.display = "none";
         }
     };
 }
