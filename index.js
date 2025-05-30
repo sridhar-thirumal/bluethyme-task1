23ba9bb437a161ab50be9e2fa3d289b5c447647b
sridhar-thirumal <sridhar.thirumal@bigthinkcode.com>
2025-05-30 13:32:25 +0530

    Made proposed changes

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
-    const ham = document.querySelector("#ham-svg");
-    const cross = document.querySelector("#close-svg");
-    const nav = document.querySelector("#nav-list");
-    document.querySelector("#menu-button").onclick = function() {
-        if(ham.style.display === "block") {
-            ham.style.display = "none";
-            cross.style.display = "block";
-            nav.style.display = "flex";
-        }
-        else {
-            ham.style.display = "block";
-            cross.style.display = "none";
-            nav.style.display = "none";
-        }
-    };
+    const ham = document.getElementById("ham-svg");
+    const cross = document.getElementById("close-svg");
+    const nav = document.getElementById("nav-list");
+
+    document.getElementById("menu-button").addEventListener("click", displayNavList);
 }
+
+function displayNavList() {
+    const ham = document.getElementById("ham-svg");
+    const cross = document.getElementById("close-svg");
+    const nav = document.getElementById("nav-list");
+
+    if(ham.style.display === "block") {
+        ham.style.display = "none";
+        cross.style.display = "block";
+        nav.style.display = "flex";
+    }
+    else {
+        ham.style.display = "block";
+        cross.style.display = "none";
+        nav.style.display = "none";
+    }
+};
